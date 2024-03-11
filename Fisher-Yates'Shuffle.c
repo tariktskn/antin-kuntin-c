0c4febb61ab2d69b0e5dce71fde01862493c9243
Tarık <130666177+tariktskn@users.noreply.github.com>
2024-03-11 22:04:00 +0300

    Add files via upload

--- Fisher-Yates'Shuffle.c
+++ Fisher-Yates'Shuffle.c
@@ -3,6 +3,7 @@
 #include <time.h>
 
 void shuffle(int array[], int N);
+int compare(int *array, int* originalArray, int N);
 void printArray(int* array, int N);
 int* setArray(int N);
 void freeArray(int *array);
@@ -10,21 +11,34 @@ void freeArray(int *array);
 int main(){
 	int N = 10, i;
 	int* array = setArray(N);
+	int* originalArray = setArray(N);
+	
+	srand(time(NULL));
 	
 	printf("Original Array: ");
-	printArray(array, N);
-	for(i=0;i<10;i++){
-		shuffle(array, N);
-		printArray(array, N);
-		sleep(1); // if it doesn't sleep, after N times it becomes first state
-	}
+	printArray(originalArray, N);
 	
+//	for(i=0;i<10;i++){
+//		shuffle(array, N);
+//		printArray(array, N);
+////		sleep(1); // if it doesn't sleep, after N times it becomes first state -> #FIX# because of srand() in shuffle function not in main function
+//	}
+
+	int count = 0;	
+	do{
+		count++;
+		shuffle(array, N);
+//		printf("Shuffled Array: ");
+//		printArray(array, N);
+	}while(!compare(array, originalArray, N));
+	printf("Shuffled array equals original array after %d shuffles.", count);
+	
+	freeArray(originalArray);
 	freeArray(array);
 	return 0;
 }
 
 void shuffle(int array[], int N){
-	srand(time(NULL));
 	int index, temp;
 		
 	int i;
@@ -34,6 +48,20 @@ void shuffle(int array[], int N){
 		array[i] = array[index];
 		array[index] = temp;
 	}
+}
+
+int compare(int *array, int* originalArray, int N){
+	int i=0;
+	while(i<N && array[i] == originalArray[i]){
+		i++;
+	}
+	
+	if(i==N){
+		return 1;
+	}else{
+		return 0;
+	}
+	
 }
 
 void freeArray(int *array){
